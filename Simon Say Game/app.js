684445377bfcdd2fccf06010e77cf0faf9fc3cd8
Saurabh244921 <saurabhkumar244921@gmail.com>
2026-03-03 09:41:31 +0530

    add check

--- Simon Say Game/app.js
+++ Simon Say Game/app.js
@@ -19,18 +19,34 @@ function btnFlash(btn){
     }, 200);
 }
 
+function checkAns(idx){
+    if(gamePattern[idx] === userPattern[idx]){
+        if(userPattern.length == gamePattern.length){
+            setTimeout(levelUp, 1000);
+        }
+    } else {
+        h2.innerText = "Game Over! Press any key to restart";
+    }
+}
+
 function levelUp(){
+    userPattern = [];
     level++;
     h2.innerText = `Level ${level}`;
-    let randIdx = Math.floor(Math.random()*3);
+    let randIdx = Math.floor(Math.random()*4);
     let randColour = buttonColours[randIdx];
     let randBtn = document.querySelector(`.${randColour}`);
+    gamePattern.push(randColour);
     btnFlash(randBtn);
 }
 
 function btnPress(){
 let btn = this;
 btnFlash(btn);
+let userbtn = btn.getAttribute("id");
+userPattern.push(userbtn);
+console.log(userPattern);
+checkAns(userPattern.length - 1);
 }
 
 let allBtns = document.querySelectorAll(".btn");
